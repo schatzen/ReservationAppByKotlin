19913ea2bb0abcf7ed793fc31bec3a2043601999
NightOwl <steelman2019e@gmail.com>
2020-03-21 19:53:14 +0900

    Create RetrofitService.kt
    Retrofit interface 양식.

---
+++ client/app/src/main/java/com/t304/our_neighborhood/service/retrofit2/RetrofitService.kt
@@ -0,0 +1,13 @@
+package com.t304.our_neighborhood.service.retrofit2
+
+interface RetrofitService {
+    /*@Headers("Authorization: ${author.authorization}")
+    @GET("v2/local/search/keyword")
+    suspend fun requestSearchLocal(
+        @Query("query") keyword: String,
+        @Query("x") x: Double,
+        @Query("y") y: Double,
+        @Query("radius") radius: Int,
+        @Query("page") page: Int
+    ): KakaoLocalSearchModel*/
+}
