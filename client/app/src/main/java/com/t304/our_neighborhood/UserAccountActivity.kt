615b5f7b73fddce30fe59b138f7ec9a2deca31d1
NightOwl <steelman2019e@gmail.com>
2020-03-21 19:53:38 +0900

    Create UserAccountActivity.kt
    유저 계정과 연관된 Fragment를 보유하는 Activity.

---
+++ client/app/src/main/java/com/t304/our_neighborhood/UserAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.t304.our_neighborhood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_account)
+    }
+}
