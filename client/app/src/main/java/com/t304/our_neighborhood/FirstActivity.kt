044ff1ef29bfcca8da761d6860437f5e27128c69
nightowl <fkwjddn@gmail.com>
2020-03-23 17:30:38 +0900

    rename activity : main -> first

---
+++ client/app/src/main/java/com/t304/our_neighborhood/FirstActivity.kt
@@ -0,0 +1,12 @@
+package com.t304.our_neighborhood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+    }
+}
