3be9f255c26cef6237734ef97ac3f904d541ad88
nightowl <fkwjddn@gmail.com>
2020-03-23 17:32:40 +0900

    + HomeActivity

--- client/app/src/main/java/com/t304/our_neighborhood/HomeActivity.kt
+++ client/app/src/main/java/com/t304/our_neighborhood/HomeActivity.kt
@@ -2,12 +2,11 @@ package com.t304.our_neighborhood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
