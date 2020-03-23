db7b771b8743d8e6f369e78a94a1f4baf48fb91d
nightowl <fkwjddn@gmail.com>
2020-03-23 17:37:09 +0900

    rename - preloader -> Preloader

--- client/app/src/main/java/com/t304/our_neighborhood/fragment/Preloader.kt
+++ client/app/src/main/java/com/t304/our_neighborhood/fragment/Preloader.kt
@@ -9,7 +9,7 @@ import androidx.navigation.fragment.findNavController
 import com.t304.our_neighborhood.R
 import kotlinx.android.synthetic.main.fragment_preloader.view.*
 
-class preloader : Fragment() {
+class Preloader : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_preloader, container, false)
