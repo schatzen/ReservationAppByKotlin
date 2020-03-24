7f5732db9cd67f6c258f161f69f699c2b43ef266
nightowl <fkwjddn@gmail.com>
2020-03-24 15:20:21 +0900

    + MapView추가, 스크롤 이벤트 추가. 스크롤 이벤트 : 터치중에 하단 버튼이 안보이게하여 전체적으로 넓어보이게끔 수정.

--- client/app/src/main/java/com/t304/our_neighborhood/fragment/DetailFragment.kt
+++ client/app/src/main/java/com/t304/our_neighborhood/fragment/DetailFragment.kt
@@ -1,16 +1,17 @@
 package com.t304.our_neighborhood.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.viewpager2.widget.ViewPager2
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
 import com.t304.our_neighborhood.R
 import com.t304.our_neighborhood.adapter.DetailViewPagerAdapter
 import kotlinx.android.synthetic.main.fragment_detail.view.*
@@ -19,6 +20,13 @@ class DetailFragment : Fragment(), OnMapReadyCallback {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_detail, container, false)
+
+        // init mapView
+        v.mapView.apply {
+            onCreate(savedInstanceState)
+            onResume()
+            getMapAsync(this@DetailFragment)
+        }
 
         //init toolbar
         v.toolbar_detail.apply {
@@ -32,15 +40,34 @@ class DetailFragment : Fragment(), OnMapReadyCallback {
             orientation = ViewPager2.ORIENTATION_HORIZONTAL
         }
 
+        v.scrollview.setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN ->
+                    v.btn_detail_reservate.apply {
+                        animate().translationY(height.toFloat())
+                        visibility = View.GONE
+                    }
+                MotionEvent.ACTION_UP ->
+                    v.btn_detail_reservate.apply {
+                        animate()
+                            .translationY(0f)
+                            .duration = 130
+                        visibility = View.VISIBLE
+                    }
+            }
+            false
+        }
+
         return v
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-    }
-
     override fun onMapReady(p0: GoogleMap?) {
+        val seoul = LatLng(37.566, 126.978)
+        
+        p0?.run {
+            moveCamera(CameraUpdateFactory.newLatLng(seoul))
+            moveCamera(CameraUpdateFactory.zoomTo(10f))
+        }
 
     }
 }
