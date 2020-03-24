9a96d7d46210aa6e9d234dfc1b05324005cdc6ad
nightowl <fkwjddn@gmail.com>
2020-03-24 19:16:12 +0900

    -u util.kt - +키보드 내리는 전역함수

---
+++ client/app/src/main/java/com/t304/our_neighborhood/util/util.kt
@@ -0,0 +1,12 @@
+package com.t304.our_neighborhood.util
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+fun hideKeyBoard(targetView: View, context: Context?) =
+    (context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+        .hideSoftInputFromWindow(
+            targetView.windowToken,
+            0
+        )
