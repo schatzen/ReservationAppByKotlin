779f43e6a04149787ec5a8ad2c17d890eaa990b7
nightowl <fkwjddn@gmail.com>
2020-03-24 19:14:42 +0900

    -u SingUp,Login Fragment - 배경 터치시 키보드 내림

--- client/app/src/main/java/com/t304/our_neighborhood/fragment/LoginFragment.kt
+++ client/app/src/main/java/com/t304/our_neighborhood/fragment/LoginFragment.kt
@@ -5,16 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.navigation.fragment.findNavController
 import com.t304.our_neighborhood.R
+import com.t304.our_neighborhood.util.hideKeyBoard
 import kotlinx.android.synthetic.main.fragment_login.view.*
 
 class LoginFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_login, container, false)
-        v.btn_login_go_signup.setOnClickListener { findNavController().navigate(R.id.action_loginFragment_to_signUpFragment) }
-        v.btn_login_submit.setOnClickListener { findNavController().navigate(R.id.action_loginFragment_to_nav_graph_home) }
+        activity?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+        v.apply {
+            setOnTouchListener { _, _ -> hideKeyBoard(v.findFocus(), activity); false }
+            btn_login_go_signup.setOnClickListener { findNavController().navigate(R.id.action_loginFragment_to_signUpFragment) }
+            btn_login_submit.setOnClickListener { findNavController().navigate(R.id.action_loginFragment_to_nav_graph_home) }
+        }
         return v
     }
 
--- client/app/src/main/java/com/t304/our_neighborhood/fragment/SignUpFragment.kt
+++ client/app/src/main/java/com/t304/our_neighborhood/fragment/SignUpFragment.kt
@@ -1,16 +1,21 @@
 package com.t304.our_neighborhood.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.Fragment
 import com.t304.our_neighborhood.R
+import com.t304.our_neighborhood.util.hideKeyBoard
 
 class SignUpFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_sign_up, container, false)
+        activity?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
+        v.setOnTouchListener { _, _ -> hideKeyBoard(v.findFocus(), activity); false }
         return v
     }
+
 }
