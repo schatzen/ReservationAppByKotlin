2e9a5b98b8b21e0ddb9f475a28035fb2a3aa09b6
nightowl <fkwjddn@gmail.com>
2020-03-25 18:59:13 +0900

    -u preloader.kt - 서버연동 테스트 코드

--- client/app/src/main/java/com/t304/our_neighborhood/fragment/Preloader.kt
+++ client/app/src/main/java/com/t304/our_neighborhood/fragment/Preloader.kt
@@ -1,12 +1,16 @@
 package com.t304.our_neighborhood.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.t304.our_neighborhood.R
+import com.t304.our_neighborhood.viewModel.UserAccountViewModel
 import kotlinx.android.synthetic.main.fragment_preloader.view.*
 
 class Preloader : Fragment() {
@@ -14,6 +18,13 @@ class Preloader : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_preloader, container, false)
         v.btn_perloader_go_login.setOnClickListener { findNavController().navigate(R.id.action_preloader_to_user_account_nav_graph) }
+
+        //test TODO DELETE
+        val viewModel = ViewModelProvider(this)[UserAccountViewModel::class.java]
+        viewModel.resultOfInsert.observe(viewLifecycleOwner, Observer { Log.d("ljw", it.toString()) })
+        viewModel.signUp()
+
         return v
     }
+
 }
