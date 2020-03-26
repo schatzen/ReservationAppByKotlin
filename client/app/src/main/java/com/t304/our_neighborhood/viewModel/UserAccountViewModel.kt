5272506c1ecb4e45f4a8bde66ea97dcd9c4bdc4c
nightowl <fkwjddn@gmail.com>
2020-03-26 11:19:14 +0900

    -u UserAccountViewModel - 테스트 코드 제거.

--- client/app/src/main/java/com/t304/our_neighborhood/viewModel/UserAccountViewModel.kt
+++ client/app/src/main/java/com/t304/our_neighborhood/viewModel/UserAccountViewModel.kt
@@ -1,30 +1,25 @@
 package com.t304.our_neighborhood.viewModel
 
-import android.accounts.NetworkErrorException
-import android.app.Application
 import android.util.Log
-import android.widget.Toast
-import androidx.lifecycle.*
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.t304.our_neighborhood.error.ErrorResponse
 import com.t304.our_neighborhood.service.retrofit2.RetrofitHelper
 import com.t304.our_neighborhood.vo.UserVo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.lang.Exception
 import java.net.ConnectException
 
 class UserAccountViewModel : ViewModel() {
     var resultOfInsert = MutableLiveData<Map<String, String>>()
 
-    //test Model TODO DELETE
-    var test1 = UserVo(member_name = "la", member_id = "la_ID", member_pwd = "1234", member_tel = "000-0000-0000")
-
-    fun signUp() {
+    fun signUp(userVo : UserVo) {
         viewModelScope.launch(Dispatchers.IO) {
             RetrofitHelper.getService().run {
                 try {
-                    resultOfInsert.postValue(withContext(Dispatchers.IO) { insertUser(test1) })
+                    resultOfInsert.postValue(withContext(Dispatchers.IO) { insertUser(userVo) })
                 } catch (e: ConnectException) {
                     // server connect faild exception
                     Log.d("ljw", "server connect failed")
