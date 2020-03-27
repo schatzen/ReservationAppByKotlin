36b783a1cc236a0b3aa5d6f80693d6391ba0097d
schatzen <56854068+schatzen@users.noreply.github.com>
2020-03-27 10:51:46 +0900

    Update LoginFragment.kt
    ViewModel과 연동, 로그인 submit 시 데이터 보내기

--- client/app/src/main/java/com/t304/our_neighborhood/fragment/LoginFragment.kt
+++ client/app/src/main/java/com/t304/our_neighborhood/fragment/LoginFragment.kt
@@ -6,9 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.t304.our_neighborhood.R
 import com.t304.our_neighborhood.util.hideKeyBoard
+import com.t304.our_neighborhood.viewModel.UserAccountViewModel
+import com.t304.our_neighborhood.vo.UserVo
 import kotlinx.android.synthetic.main.fragment_login.view.*
 
 class LoginFragment : Fragment() {
@@ -19,9 +24,24 @@ class LoginFragment : Fragment() {
         v.apply {
             setOnTouchListener { _, _ -> hideKeyBoard(v.findFocus(), activity); false }
             btn_login_go_signup.setOnClickListener { findNavController().navigate(R.id.action_loginFragment_to_signUpFragment) }
-            btn_login_submit.setOnClickListener { findNavController().navigate(R.id.action_loginFragment_to_nav_graph_home) }
+            //btn_login_submit.setOnClickListener { findNavController().navigate(R.id.action_loginFragment_to_nav_graph_home) }
+        }
+
+        val viewModel = ViewModelProvider(this)[UserAccountViewModel::class.java]
+        viewModel.resultOfLogin.observe(viewLifecycleOwner, Observer { Toast.makeText(context, it.toString(), Toast.LENGTH_SHORT).show() })
+
+
+        // 보내기
+        v.btn_login_submit.setOnClickListener {
+            with(v) {
+                viewModel.singIn(
+                    UserVo(
+                        member_id = tiet_login_id.text.toString(),
+                        member_pwd = tiet_login_pw.text.toString()
+                    )
+                )
+            }
         }
         return v
     }
-
 }
