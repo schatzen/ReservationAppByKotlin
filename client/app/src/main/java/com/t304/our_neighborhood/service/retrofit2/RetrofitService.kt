2ef933df0eccec3c5166f823d951d3f36e5fc9d5
schatzen <56854068+schatzen@users.noreply.github.com>
2020-03-27 10:49:30 +0900

    Update RetrofitService.kt
    SignUp과 SignIn URI 수정

--- client/app/src/main/java/com/t304/our_neighborhood/service/retrofit2/RetrofitService.kt
+++ client/app/src/main/java/com/t304/our_neighborhood/service/retrofit2/RetrofitService.kt
@@ -2,6 +2,7 @@ package com.t304.our_neighborhood.service.retrofit2
 
 import com.t304.our_neighborhood.vo.UserVo
 import retrofit2.http.*
+import java.util.*
 
 interface RetrofitService {
     /*@Headers("Authorization: ${author.authorization}")
@@ -14,8 +15,11 @@ interface RetrofitService {
         @Query("page") page: Int
     ): KakaoLocalSearchModel*/
 
-    @POST("reserv/member")
-    suspend fun insertUser(
-        @Body vo: UserVo
-    ) : Map<String, String>
+    // sign up
+    @POST("reserv/member/signup")
+    suspend fun insertUser(@Body vo: UserVo): Map<String, String>
+
+    // sign in
+    @POST("reserv/member/signin")
+    suspend fun getUser(@Body vo: UserVo): UserVo
 }
