d048cedbf251fd92d7dfdd4e43c0bc51d9cf159d
schatzen <56854068+schatzen@users.noreply.github.com>
2020-03-27 10:47:27 +0900

    Update UserAccountViewModel.kt
    로그인(SingIn) VIewModel 추가

--- client/app/src/main/java/com/t304/our_neighborhood/viewModel/UserAccountViewModel.kt
+++ client/app/src/main/java/com/t304/our_neighborhood/viewModel/UserAccountViewModel.kt
@@ -5,15 +5,20 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.t304.our_neighborhood.error.ErrorResponse
+import com.t304.our_neighborhood.service.SessionUser
 import com.t304.our_neighborhood.service.retrofit2.RetrofitHelper
 import com.t304.our_neighborhood.vo.UserVo
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import okhttp3.Dispatcher
 import java.net.ConnectException
+import java.util.*
 
 class UserAccountViewModel : ViewModel() {
     var resultOfInsert = MutableLiveData<Map<String, String>>()
+    var resultOfLogin = MutableLiveData<Boolean>()
 
     fun signUp(userVo : UserVo) {
         viewModelScope.launch(Dispatchers.IO) {
@@ -27,6 +32,26 @@ class UserAccountViewModel : ViewModel() {
                 }
             }
         }
+
+    }
+
+    fun singIn(userVo : UserVo) {
+        viewModelScope.launch(Dispatchers.IO) {
+           RetrofitHelper.getService().run {
+               try {
+                   //val vo = withContext(Dispatchers.IO) { getUser(userVo) } // return vo
+                   //SessionUser.user = vo
+                   //resultOfLogin.postValue(withContext(Dispatchers.IO) { getUser(userVo) })
+                   val test = withContext(Dispatchers.IO) { getUser(userVo) }
+                   // if test ? not null resultOfLogin =true
+                   Log.d("ljw", test.toString())
+               } catch (e: ConnectException) {
+                   // server connect faild exception
+                   Log.d("ljw", "server login failed")
+                   ErrorResponse.response.postValue(ErrorResponse.SERVER_CONNECT_FAILED)
+               }
+           }}
+
 
     }
 
